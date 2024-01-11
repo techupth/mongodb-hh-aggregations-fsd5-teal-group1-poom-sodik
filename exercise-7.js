671bbda89abfad03e24daae0b027b0667e53a9d2
Poom <kittipoom.put@gmail.com>
2024-01-11 13:12:53 +0700

    finish

--- exercise-7.js
+++ exercise-7.js
@@ -1,1 +1,18 @@
 // Paste the complete MQL here
+db.pizzaOrders.aggregate([
+  {
+    $group: {
+      _id: {
+        year: { $year: "$created_at" },
+        month: { $month: "$created_at" },
+      },
+      total_sales_by_month: { $sum: "$total_price" },
+    },
+  },
+  {
+    $sort: {
+      total_sale_by_month: 1,
+    },
+  },
+  { $limit: 1 },
+]);
