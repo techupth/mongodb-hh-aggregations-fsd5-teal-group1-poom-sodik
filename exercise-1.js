be84d8343046b7c22b613fcd8ac33628cb8836dd
Sodikcp <paramesh.rah@gmail.com>
2024-01-11 14:08:22 +0700

    feat:complete MongoDB Aggregation

--- exercise-1.js
+++ exercise-1.js
@@ -1,1 +1,12 @@
 // Paste the complete MQL here
+db.pizzaOrders.aggregate([
+  { $group: { _id: "$credit_card_type", totalpaid: { $sum: "$total_price" } } },
+]);
+// {
+//     _id: null,
+//     totalpaid: 3675211.52
+//   }
+//   {
+//     _id: 'mastercard',
+//     totalpaid: 1432896.81
+//   }
